570426d5d8f76e3adf82628df8259e31df3c82a0
Johannes Bertsch <johannes.bertsch@student.kit.edu>
2026-05-12 15:11:00 +0200

    counting implemented

--- YOLO/project.py
+++ YOLO/project.py
@@ -1,7 +1,10 @@
+from unittest import case
+
 import numpy as np
 import cv2 as cv
 import os
 import argparse
+from scipy.optimize import linear_sum_assignment
 
 # Check OpenCV version
 opencv_python_version = lambda str_version: tuple(map(int, (str_version.split("."))))
@@ -9,12 +12,6 @@ assert opencv_python_version(cv.__version__) >= opencv_python_version("4.10.0"),
        "Please install latest opencv-python for benchmark: python3 -m pip install --upgrade opencv-python"
 
 from ultralytics import YOLO
-
-
-
-
-
-
 
 
 def keypoint_extractor(box_corners, frame, frame_prev,keypoints_prev, keypoints_descriptors_prev, fps=None, th=10):
@@ -33,21 +30,8 @@ def keypoint_extractor(box_corners, frame, frame_prev,keypoints_prev, keypoints_
 
 
     #mask static keypoints
-    keypoints = mask_static_keypoints(box_corners, frame, frame_prev, keypoints,  th=10)
-
-    ##FLATTENED_KEYPOINTS
-    # keypoints_flattend = [kp for sublist in keypoints for kp in sublist]
-    # #keypoints_prev_flattend = [kp for sublist in keypoints_prev for kp in sublist]
-    # keypoints, frame_descriptors = desc_extractor.compute(frame, keypoints_flattend)
-    # if (
-    # frame_descriptors is not None and
-    # keypoints_descriptors_prev is not None and
-    # len(frame_descriptors) > 0 and
-    # len(keypoints_descriptors_prev) > 0):
-    #     matches = matcher.knnMatch(frame_descriptors, keypoints_descriptors_prev, k=2)
-    #     good_matches = extract_good_ratio_matches(matches, max_ratio=0.8)
-    # else:
-    #     good_matches = []
+    keypoints_unmasked = keypoints.copy()
+    keypoints = mask_static_keypoints(box_corners, frame, frame_prev, keypoints,  th=20)
 
 
     #iterate over combinations of bboxes
@@ -72,7 +56,7 @@ def keypoint_extractor(box_corners, frame, frame_prev,keypoints_prev, keypoints_
         
     #print(keypoints)
 
-    return keypoints, frame_descriptors, good_matches
+    return keypoints, frame_descriptors, good_matches, keypoints_unmasked
 
 def mask_static_keypoints(box_corners,frame,frame_prev,keypoints,th=10):
     ret=frame.copy()
@@ -145,11 +129,11 @@ def vis(box_corners, confs,res_img, keypoints, fps=None, keypoints_unmasked=None
         label = "person {:.2f}".format(confs[i])
         cv.putText(ret, label, (xmin, ymin - 10), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), thickness=2)
 
-        # keypoint_bbox_unmasked = keypoints_unmasked[i]
-        # for keypoint in keypoint_bbox_unmasked:
-        #     x, y = keypoint.pt[0]+xmin, keypoint.pt[1]+ymin
-        #     #print(f"Keypoint coordinates: x={x}, y={y}, shape_img={ret.shape}")
-        #     cv.circle(ret, (int(x), int(y)), 3, (0, 0, 255), -1)
+        keypoint_bbox_unmasked = keypoints_unmasked[i]
+        for keypoint in keypoint_bbox_unmasked:
+            x, y = keypoint.pt[0]+xmin, keypoint.pt[1]+ymin
+            #print(f"Keypoint coordinates: x={x}, y={y}, shape_img={ret.shape}")
+            cv.circle(ret, (int(x), int(y)), 3, (0, 0, 255), -1)
         
 
         keypoint_bbox = keypoints[i]
@@ -163,11 +147,11 @@ def vis(box_corners, confs,res_img, keypoints, fps=None, keypoints_unmasked=None
     return ret
 
 
-def vis_matches(box_corner,frame, keypoints, matches, keypoints_prev, letterbox_scale, i, j):
+def vis_matches(box_corner,frame, keypoints, matches, keypoints_prev,  i, j):
     
     ret = frame.copy()
 
-    #for (xmin, ymin, xmax, ymax) in box_corner:
+    xmin, ymin, xmax, ymax=box_corner[i]
 
     
     for match in matches:
@@ -216,6 +200,7 @@ if __name__=='__main__':
         cwd = os.getcwd()
         parent = os.path.abspath(os.path.join(cwd, os.pardir))
         parent_parent=os.path.abspath(os.path.join(parent, os.pardir))
+        orientation="horizontal" #choose orientation for counting: "horizontal" or "vertical"
         example_path=f"{parent}\examples\example2.mp4"
         #print(example_path)
         cap = cv.VideoCapture(example_path)#(deviceId)
@@ -226,6 +211,9 @@ if __name__=='__main__':
         frame_prev=None
         keypoints_prev=[]
         keypoints_descriptors_prev=None
+        box_centers_prev=None
+        count_in=0
+        count_out=0
         #out=cv.VideoWriter('matching.mp4', cv.VideoWriter_fourcc(*'mp4v'), 30, (int(cap.get(cv.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))))
 
         while cv.waitKey(1) < 0:
@@ -238,9 +226,9 @@ if __name__=='__main__':
             
                  
 
-            #input_blob = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+            #frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
             
-            #input_blob, letterbox_scale = letterbox(input_blob)
+
             
             # Inference
             tm.start()
@@ -250,6 +238,7 @@ if __name__=='__main__':
             #    preds=preds[preds[:, -1] == 0]
             #print(preds)
             box_corners = []
+            box_centers = []
             confs = []
             for box in preds[0].boxes:
                 #detect only people
@@ -258,14 +247,15 @@ if __name__=='__main__':
                     xmin, ymin, xmax, ymax = map(int, box.xyxy[0])
                     conf = float(box.conf[0])
                     box_corners.append((xmin, ymin, xmax, ymax))
+                    box_centers.append(((xmin + xmax) // 2, (ymin + ymax) // 2))
                     confs.append(conf)
-            keypoints, keypoint_descriptors, good_matches = keypoint_extractor(box_corners, frame, frame_prev, keypoints_prev, keypoints_descriptors_prev,fps=tm.getFPS())
+            keypoints, keypoint_descriptors, good_matches, keypoints_unmasked = keypoint_extractor(box_corners, frame, frame_prev, keypoints_prev, keypoints_descriptors_prev,fps=tm.getFPS())
             #keypoints_masked=mask_static_keypoints(preds, frame, frame_prev, keypoints, letterbox_scale, th=10 ) 
            
             
 
             counts = np.zeros((len(keypoints), len(keypoints_prev)), dtype=int)
-            img = vis(box_corners, confs, frame, keypoints, fps=tm.getFPS())
+            img = vis(box_corners, confs, frame, keypoints, keypoints_unmasked=keypoints_unmasked, fps=tm.getFPS())
 
             for i in range(len(keypoints)):
                 for j in range(len(keypoints_prev)):
@@ -275,23 +265,90 @@ if __name__=='__main__':
                     else:
                         counts[i, j] = len(good_matches[i, j])
             
-                    #img = vis_matches(preds,img, keypoints, good_matches[i,j], keypoints_prev, letterbox_scale)
+                    #img = vis_matches(box_corners,img, keypoints, good_matches[i,j], keypoints_prev, i, j)
             
             print(f"Counts of good matches between current and previous frame: \n{counts}")
-            #TODO Better assignment between boxes (Hungarian algorithm or linear sum assignment) instead of greedy approach
-            if counts.size > 0:
-                for i in range(len(keypoints)):
-                    prev_bbox_index = np.argmax(counts[i])
-                    print(f"Best matching previous bbox index for current bbox {i}: {prev_bbox_index} with {counts[i, prev_bbox_index]} good matches")
-                    matches_to_prev_bbox = good_matches[i, prev_bbox_index]
-                    #img = vis_matches(preds,img, keypoints, matches_to_prev_bbox, keypoints_prev, i, prev_bbox_index)  
+            matches_to_prev_bbox_arr = []
 
+            if counts.size > 0:
+                  
+                    
+    
+                # Hungarian minimizes cost, so maximize matches via negative counts
+                cost_matrix = -counts
+
+                # row_ind = current boxes
+                # col_ind = previous boxes
+                row_ind, col_ind = linear_sum_assignment(cost_matrix)
+
+                print("\nOptimal assignments:")
+
+                for curr_idx, prev_idx in zip(row_ind, col_ind):
+
+                    match_count = counts[curr_idx, prev_idx]
+
+                    # Optional threshold to reject weak matches
+                    if match_count == 0:
+                        continue
+
+                    print(
+                        f"Current bbox {curr_idx} "
+                        f"matched with previous bbox {prev_idx} "
+                        f"({match_count} matches)"
+                    )
+
+                    matches_to_prev_bbox = good_matches[curr_idx, prev_idx]
+                    matches_to_prev_bbox_arr.append(matches_to_prev_bbox)
+                    
+                    img = vis_matches(box_corners,img, keypoints, matches_to_prev_bbox, keypoints_prev, curr_idx, prev_idx)
+
+            match orientation:
+                case "horizontal":
+                    img=cv.line(img, (0, img.shape[0]//2), (img.shape[1], img.shape[0]//2), (255, 255, 255), thickness=2) #horizontal line for counting
+                    if box_centers_prev is not None:
+                        for i, center in enumerate(box_centers):
+                            for j, prev_center in enumerate(box_centers_prev):
+                                dist = np.linalg.norm(np.array(center) - np.array(prev_center))
+                                if dist < 50:  # distance threshold
+                                    print(f"Box {i} in current frame is close to box {j} in previous frame (distance: {dist:.2f})")
+                                    if center[1] < img.shape[0]//2 and prev_center[1] >= img.shape[0]//2:
+                                        count_in += 1
+                                        print(f"Counted IN: Box {i} moved from below to above the line.")
+                                    elif center[1] >= img.shape[0]//2 and prev_center[1] < img.shape[0]//2:
+                                        count_out += 1
+                                        print(f"Counted OUT: Box {i} moved from above to below the line.")
+            
+                case "vertical":
+                    img=cv.line(img, (img.shape[1]//2, 0), (img.shape[1]//2, img.shape[0]), (255, 255, 255), thickness=2) #vertical line for counting
+                    if box_centers_prev is not None:
+                        for i, center in enumerate(box_centers):
+                            for j, prev_center in enumerate(box_centers_prev):
+                                dist = np.linalg.norm(np.array(center) - np.array(prev_center))
+                                if dist < 50:  # distance threshold
+                                    print(f"Box {i} in current frame is close to box {j} in previous frame (distance: {dist:.2f})")
+                                    if center[0] < img.shape[1]//2 and prev_center[0] >= img.shape[1]//2:
+                                        count_in += 1
+                                        print(f"Counted IN: Box {i} moved from right to left of the line.")
+                                    elif center[0] >= img.shape[1]//2 and prev_center[0] < img.shape[1]//2:
+                                        count_out += 1
+                                        print(f"Counted OUT: Box {i} moved from left to right of the line.")
+                case _:
+                    print("Invalid orientation for counting. Please choose 'horizontal' or 'vertical'.")
+
+            #counting by comparing box centers to previous frame
+            
             #print(preds)
+            label = f"IN: {count_in}  OUT: {count_out}"
+            img=cv.putText(img, label, (10, 75), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+
             cv.imshow("NanoDet Demo", img)
+            
             
             #out.write(img)#save video
             frame_prev=frame.copy()
             keypoints_prev=keypoints
             keypoints_descriptors_prev=keypoint_descriptors
+            box_centers_prev=box_centers
 
             tm.reset()
